6d0b15448d68e5e10fbdc69f898a3cea14348f7f
Ismat-Samadov <ismetsemedov@gmail.com>
2024-03-18 01:01:27 +0400

    check

--- src/App.js
+++ src/App.js
@@ -1,27 +1,6 @@
-// // App.js
-// import React from 'react';
-// import PostList from './components/PostList';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <h1>Welcome to My Blog</h1>
-//       </header>
-//       <main>
-//         <PostList />
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
+// App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PostList from './components/PostList';
-import WriteArticlePage from './components/WriteArticlePage';
-import ArticleDetailPage from './components/ArticleDetailPage';
 
 function App() {
   return (
@@ -29,15 +8,10 @@ function App() {
       <header className="App-header">
         <h1>Welcome to My Blog</h1>
       </header>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={PostList} />
-          <Route exact path="/write-article" component={WriteArticlePage} />
-          <Route exact path="/article/:id" component={ArticleDetailPage} />
-        </Switch>
-      </Router>
+      <main>
+        <PostList />
+      </main>
     </div>
   );
 }
 
-export default App;
